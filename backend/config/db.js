cb47d40eef80b44776556db43f3b99f2c9d0cf20
Flash191102 <79019475+Flash191102@users.noreply.github.com>
2024-09-18 19:53:23 +0530

    first commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,13 @@
-const mongoose = require( "mongoose" );
-const dotenv = require( "dotenv" );
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 dotenv.config();
 const connectDB = async () => {
     try {
-        await mongoose.connect( process.env.MONGO_URL );
-        console.log( `connected to MongoDB Database ${mongoose.connection.host}` );
-    } catch ( error ) {
-        console.log( `Mongodb Database Error ${error}` );
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log(`connected to MongoDB Database ${mongoose.connection.host}`);
+    } catch (error) {
+        console.log(`Mongodb Database Error ${error}`);
     }
 };
 module.exports = connectDB;
